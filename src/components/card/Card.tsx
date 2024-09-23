3eab9a5de49e191adc2aad213a1c3fe586c600cb
Tofaal9152 <tofaal91522@gmail.com>
2024-09-24 03:01:46 +0600

    first commit

---
+++ src/components/card/Card.tsx
@@ -0,0 +1,20 @@
+import IconButton from "../iconButton/IconButton";
+import { FaSearch } from "react-icons/fa";
+
+export default function Card({ image, price, title, callback }: { image: string, price: number, title: string, callback: () => void }) {
+    return (
+        <div className="flex flex-col bg-[#ffffffcd] p-4 rounded-xl shadow-lg mt-2 mb-2">
+            <div className="h-40 w-40 bg-cover bg-center rounded-md" style={{
+                backgroundImage: `url(${image})`
+            }}></div>
+            <span className="font-bold mt-2">
+                {title}
+            </span>
+            <span className="text-xs mt-1">
+                Average Price: <br />
+                ৳{price} Taka
+            </span>
+            <IconButton direction="right" icon={<FaSearch />} text="Visit" callback={callback} />
+        </div>
+    );
+}
---
+++ src/pages/customerDashboard/mainDashboard.tsx
@@ -0,0 +1,74 @@
+import Header from "../../components/header/Header";
+import NavigationBar from "../../components/navigationMenu/NavigationMenu";
+import { RiLoginCircleLine } from "react-icons/ri";
+import { FaArrowAltCircleRight } from "react-icons/fa";
+import { useEffect, useState } from "react";
+import ExploreSalons from "./ExploreSalons";
+import bg from '../../assets/logRegBG.svg';
+import { useNavigate } from "react-router-dom";
+import { removeFromLocalStorage } from "../../utils/localStorage";
+
+export function MainDashboard() {
+    const [pageIndex, setPageIndex] = useState(0);
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        // logout
+        if (pageIndex == 3) {
+            removeFromLocalStorage("token");
+            removeFromLocalStorage("role");
+            removeFromLocalStorage("id");
+            navigate("/");
+        }
+    }, [pageIndex]);
+
+    return (
+        <>
+            <Header />
+            <div className="relative w-[80vw] m-auto">
+                <div className="flex flex-row space-x-4">
+                    <NavigationBar
+                        navMenuContents={[
+                            {
+                                title: "Explore Salons",
+                                icon: <FaArrowAltCircleRight className="w-6 h-6" />,
+                            },
+                            {
+                                title: "Appointments",
+                                icon: <RiLoginCircleLine className="w-6 h-6" />,
+                            },
+                            {
+                                title: "History",
+                                icon: <RiLoginCircleLine className="w-6 h-6" />,
+                            },
+                            {
+                                title: "Logout",
+                                icon: <RiLoginCircleLine className="w-6 h-6" />,
+                            }
+                        ]}
+                        setIndex={setPageIndex}
+                        index={pageIndex}
+                    />
+                    <div className="h-[86vh] flex-grow border-[#272727a2] border-2 rounded-xl relative mt-4" style={{
+                        background: `url(${bg})`,
+                        backgroundSize: "contain",
+                        backgroundPosition: "center"
+                    }}>
+                        {/* Container */}
+                        <div className="glass-bg h-full overflow-y-scroll no-scrollbar" style={{
+                            backgroundColor: "#aac1ff21"
+                        }}>
+                            {
+                                pageIndex == 0 ?
+                                    <ExploreSalons /> :
+                                    pageIndex == 1 ?
+                                        <></> : <></>
+                            }
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
+
+    );
+}
